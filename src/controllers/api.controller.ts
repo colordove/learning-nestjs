c4493c8ec9844022f574457dae88a8850f028f9b
Hu Jian <color.dove@gmail.com>
2019-05-21 10:36:12 +0800

    feat(app): update

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Req, Session } from '@nestjs/common';
+import svgCaptcha = require('svg-captcha');
 @Controller('api')
 export class APIController {
     @Get('token')
-    findToken() {
-        return `I'm a token!`;
+    findToken(@Req() req: Request) {
+        const captcha = svgCaptcha.create();
+        // req.session.captcha = captcha.text;
+        // app.use(session({ seccaptcharet: captcha.text }))
+        // console.log(captcha);
+        // return captcha;
     }
 }
